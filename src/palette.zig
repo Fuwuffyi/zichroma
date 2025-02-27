7bf38ad31665ea873a694d5de686cfd88404b533
Fuwuffy <palazzini21@gmail.com>
2025-02-27 14:06:29 +0100

    Improve palette generator code

--- src/palette.zig
+++ src/palette.zig
@@ -9,23 +9,30 @@ pub const Palette = struct {
 
     pub fn init(allocator: *const std.mem.Allocator, img: *const image.Image) !@This() {
         // Create list of colors
-        var colors_hashmap: std.AutoHashMap(u64, PaletteValue) = std.AutoHashMap(u64, PaletteValue).init(allocator.*);
+        var colors_hashmap: std.AutoHashMap(u128, u32) = std.AutoHashMap(u128, u32).init(allocator.*);
         defer colors_hashmap.deinit();
+        try colors_hashmap.ensureTotalCapacity(@as(u32, @intCast(img.colors.len)));
         // Loop over the image colors
         for (img.colors) |*clr| {
-            const hash: u64 = std.hash.Wyhash.hash(0, std.mem.asBytes(clr));
-            const gop = try colors_hashmap.getOrPut(hash);
+            // Convert rgba to bits and use it as key
+            const key: u128 = @bitCast(clr.*);
+            const gop = try colors_hashmap.getOrPut(key);
+            // Increase weight for that color if existing
             if (!gop.found_existing) {
-                gop.value_ptr.* = .{ .clr = clr.*, .weight = 1 };
+                gop.value_ptr.* = 1;
             } else {
-                gop.value_ptr.weight += 1;
+                gop.value_ptr.* += 1;
             }
         }
-        var colors_list = std.ArrayList(PaletteValue).init(allocator.*);
+        // Create new list to store the unique values
+        var colors_list: std.ArrayList(PaletteValue) = std.ArrayList(PaletteValue).init(allocator.*);
         defer colors_list.deinit();
+        try colors_list.ensureTotalCapacity(colors_hashmap.count());
+        // Read weight values and create the list of unique colors
         var it = colors_hashmap.iterator();
         while (it.next()) |*entry| {
-            try colors_list.append(entry.value_ptr.*);
+            const clr: color.Color = @bitCast(entry.key_ptr.*);
+            colors_list.appendAssumeCapacity(.{ .clr = clr, .weight = entry.value_ptr.* });
         }
         // Create the palette
         return .{ .values = try colors_list.toOwnedSlice() };
