d5be7a31621365c3f4a1d99247343d212b18a2cf
Fuwuffy <palazzini21@gmail.com>
2025-02-27 13:45:58 +0100

    Optimize palette generation code using hashmap

--- src/main.zig
+++ src/main.zig
@@ -9,15 +9,16 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     // Load the image
+    std.debug.print("Loading image...\n", .{});
     const img: image.Image = try image.Image.init(&allocator, "testing.png");
     defer img.deinit(&allocator);
     // Create the weighted palette from the image
+    std.debug.print("Loading palette...\n", .{});
     const pal: palette.Palette = try palette.Palette.init(&allocator, &img);
     defer pal.deinit(&allocator);
     // Do stuff
     for (pal.values) |*value| {
-        const w: u32 = value.weight;
         const col: *const color.Color = &value.clr;
-        std.debug.print("{} {} {} {} {}\n", .{ col.r, col.g, col.b, col.a, w });
+        std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0mInstances: {}\n", .{ @as(u32, @intFromFloat(col.r * 255)), @as(u32, @intFromFloat(col.g * 255)), @as(u32, @intFromFloat(col.b * 255)), value.weight });
     }
 }
--- src/palette.zig
+++ src/palette.zig
@@ -9,23 +9,26 @@ pub const Palette = struct {
 
     pub fn init(allocator: *const std.mem.Allocator, img: *const image.Image) !@This() {
         // Create list of colors
-        var colors: std.ArrayList(PaletteValue) = std.ArrayList(PaletteValue).init(allocator.*);
-        defer colors.deinit();
+        var colors_hashmap: std.AutoHashMap(u64, PaletteValue) = std.AutoHashMap(u64, PaletteValue).init(allocator.*);
+        defer colors_hashmap.deinit();
         // Loop over the image colors
-        for (img.colors) |*clr| blk: {
-            // Check if color is in colors list
-            for (colors.items) |*other| {
-                if (clr.eql(&other.clr)) {
-                    // If found increase the weight of that color
-                    other.weight += 1;
-                    break :blk;
-                }
+        for (img.colors) |*clr| {
+            const hash: u64 = std.hash.Wyhash.hash(0, std.mem.asBytes(clr));
+            const gop = try colors_hashmap.getOrPut(hash);
+            if (!gop.found_existing) {
+                gop.value_ptr.* = .{ .clr = clr.*, .weight = 1 };
+            } else {
+                gop.value_ptr.weight += 1;
             }
-            // If color not in list, add it
-            try colors.append(.{ .clr = clr.*, .weight = 1 });
+        }
+        var colors_list = std.ArrayList(PaletteValue).init(allocator.*);
+        defer colors_list.deinit();
+        var it = colors_hashmap.iterator();
+        while (it.next()) |*entry| {
+            try colors_list.append(entry.value_ptr.*);
         }
         // Create the palette
-        return .{ .values = try colors.toOwnedSlice() };
+        return .{ .values = try colors_list.toOwnedSlice() };
     }
 
     pub fn deinit(self: *const @This(), allocator: *const std.mem.Allocator) void {
