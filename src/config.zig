e230103c4652ef5a266a2cdb59858a8c6061b169
Fuwuffy <palazzini21@gmail.com>
2025-03-27 19:29:36 +0100

    Create new base config system

--- src/config.zig
+++ src/config.zig
@@ -1,46 +1,55 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-pub const Config = struct {
-    image_path: []const u8,
-    light_mode: ?bool,
+const config_file_name: []const u8 = "config.conf";
+const config_dir_name: []const u8 = "zig_colortheme_generator";
 
-    pub fn init(allocator: std.mem.Allocator, argv: [][:0]u8) !@This() {
-        // Setup empty configuration
-        var config: Config = undefined;
-        config.light_mode = null;
-        // Loop through command arguments
-        for (argv[1..]) |arg| {
-            if (std.mem.startsWith(u8, arg, "-image=")) {
-                // Set the image file path to the one passed by cli
-                config.image_path = try allocator.dupe(u8, arg["-image=".len..]);
-            } else if (std.mem.startsWith(u8, arg, "-theme=")) {
-                // Get the theme from cli
-                const theme_str: []const u8 = try allocator.dupe(u8, arg["-theme=".len..]);
-                defer allocator.free(theme_str);
-                // Set the appropriate value based on theme
-                if (std.mem.eql(u8, theme_str, "auto")) continue;
-                if (std.mem.eql(u8, theme_str, "dark")) {
-                    config.light_mode = false;
-                } else if (std.mem.eql(u8, theme_str, "light")) {
-                    config.light_mode = true;
-                } else {
-                    std.debug.print("Unknown theme: {s}\n", .{theme_str});
-                    return error.InvalidColortheme;
-                }
-            } else {
-                std.debug.print("Unknown argument: {s}\n", .{arg});
-                return error.InvalidArgument;
-            }
-        }
-        // Check if image is not set set
-        if (std.mem.eql(u8, config.image_path, "")) {
-            std.debug.print("Missing required argument: -image\n", .{});
-            return error.MissingArgument;
-        }
-        return config;
+pub fn testing(allocator: std.mem.Allocator) !void {
+    // Grab the configuration file
+    const file = try findConfigFile(allocator) orelse return error.ConfigFileNotFound;
+    defer file.close();
+    // Read the contents of the configuration file
+    const file_contents = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(file_contents);
+    // Temporairly print to console
+    std.debug.print("{s}\n", .{file_contents});
+}
+
+fn findConfigFile(allocator: std.mem.Allocator) !?std.fs.File {
+    const config_dir: []const u8 = try getConfigDir(allocator);
+    defer allocator.free(config_dir);
+    // Check config directory first
+    if (try openFileInDir(allocator, config_dir, config_file_name)) |file| return file;
+    // Otherwise check exe directory
+    const exe_dir: []const u8 = try std.fs.selfExeDirPathAlloc(allocator);
+    defer allocator.free(exe_dir);
+    return try openFileInDir(allocator, exe_dir, config_file_name);
+}
+
+fn openFileInDir(allocator: std.mem.Allocator, dir_path: []const u8, filename: []const u8) !?std.fs.File {
+    const full_path: []const u8 = try std.fs.path.join(allocator, &[_][]const u8{ dir_path, filename });
+    defer allocator.free(full_path);
+    return std.fs.openFileAbsolute(full_path, .{}) catch |err| switch (err) {
+        error.FileNotFound => null,
+        else => return err,
+    };
+}
+
+// TODO: Add other os specific config directories
+fn getConfigDir(allocator: std.mem.Allocator) ![]const u8 {
+    switch (builtin.target.os.tag) {
+        .linux => {
+            // Check XDG_CONFIG_HOME first
+            const xdg_config_dir: []const u8 = std.process.getEnvVarOwned(allocator, "XDG_CONFIG_HOME") catch |err| {
+                if (err != error.EnvironmentVariableNotFound) return err;
+                // Else check HOME variable
+                const home_dir: []const u8 = try std.process.getEnvVarOwned(allocator, "HOME");
+                defer allocator.free(home_dir);
+                return try std.fs.path.join(allocator, &[_][]const u8{ home_dir, ".config", config_dir_name});
+            };
+            defer allocator.free(xdg_config_dir);
+            return try std.fs.path.join(allocator, &[_][]const u8{ xdg_config_dir, config_dir_name});
+        },
+        else => unreachable
     }
-
-    pub fn deinit(self: *const @This(), allocator: std.mem.Allocator) void {
-        allocator.free(self.image_path);
-    }
-};
+}
--- src/main.zig
+++ src/main.zig
@@ -18,65 +18,67 @@ pub fn main() !void {
     // Read command arguments
     const argv: [][:0]u8 = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, argv);
-    const conf: config.Config = try config.Config.init(allocator, argv);
-    defer conf.deinit(allocator);
+    // Read config file
+    try config.testing(allocator);
+    
+
     // Create the weighted palette from the image or load the cache
-    std.debug.print("Loading palette...\n", .{});
-    var start: i64 = std.time.milliTimestamp();
-    const pal: palette.Palette = try cache.readPaletteCache(allocator, conf.image_path) orelse try palette.Palette.init(allocator, conf.image_path);
-    defer pal.deinit(allocator);
-    try cache.writePaletteCache(allocator, &pal);
-    std.debug.print("Creating new palette took {}ms \n", .{std.time.milliTimestamp() - start});
-    // Check if image is light or dark themed
-    const is_palette_light: bool = if (conf.light_mode != null) conf.light_mode.? else pal.isLight();
-    std.debug.print("Image is in {s} theme\n", .{if (is_palette_light) "light" else "dark"});
-    // Get clustering data
-    std.debug.print("Generating clusters...\n", .{});
-    start = std.time.milliTimestamp();
-    const clusters: []color.Color = try clustering.kmeans(allocator, &pal, 4, 50);
-    defer allocator.free(clusters);
-    std.debug.print("Generating clusters took {}ms \n", .{std.time.milliTimestamp() - start});
-    // Sort based on color theme
-    const sort_ctx = struct { light_mode: bool };
-    std.sort.block(color.Color, clusters, sort_ctx{ .light_mode = is_palette_light }, struct {
-        pub fn lessThan(ctx: sort_ctx, a: color.Color, b: color.Color) bool {
-            return if (ctx.light_mode) a.getBrightness() > b.getBrightness() else a.getBrightness() < b.getBrightness();
-        }
-    }.lessThan);
-    // Create the modulation curve for accent colors
-    const test_curve: modulation_curve.ModulationCurve = modulation_curve.ModulationCurve.init(.hsl, &.{
-        .{ .a_mod = null, .b_mod = 0.98, .c_mod = 0.09 },
-        .{ .a_mod = null, .b_mod = 0.94, .c_mod = 0.16 },
-        .{ .a_mod = null, .b_mod = 0.90, .c_mod = 0.25 },
-        .{ .a_mod = null, .b_mod = 0.82, .c_mod = 0.30 },
-        .{ .a_mod = null, .b_mod = 0.67, .c_mod = 0.42 },
-        .{ .a_mod = null, .b_mod = 0.68, .c_mod = 0.62 },
-        .{ .a_mod = null, .b_mod = 0.76, .c_mod = 0.75 },
-        .{ .a_mod = null, .b_mod = 0.92, .c_mod = 0.87 },
-    });
-    // Do stuff
-    for (clusters) |*col| {
-        // Primary color
-        const col_rgb: color.Color = col.toRGB();
-        std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m", .{ @as(u32, @intFromFloat(col_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_rgb.rgb.b * 255)) });
-        // Accent colors
-        const new_cols: []color.Color = try test_curve.applyCurve(allocator, col);
-        defer allocator.free(new_cols);
-        std.sort.block(color.Color, new_cols, sort_ctx{ .light_mode = is_palette_light }, struct {
-            pub fn lessThan(ctx: sort_ctx, a: color.Color, b: color.Color) bool {
-                return if (ctx.light_mode) a.getBrightness() > b.getBrightness() else a.getBrightness() < b.getBrightness();
-            }
-        }.lessThan);
-        for (new_cols) |*col_acc| {
-            const col_acc_rgb: color.Color = col_acc.toRGB();
-            std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m", .{ @as(u32, @intFromFloat(col_acc_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_acc_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_acc_rgb.rgb.b * 255)) });
-        }
-        // Text color
-        var col_neg_hsl: color.Color = col.negative().toHSL();
-        col_neg_hsl.hsl.s = 0.1;
-        col_neg_hsl.hsl.l = if (is_palette_light) 0.01 else 0.99;
-        const col_neg_rgb: color.Color = col_neg_hsl.toRGB();
-        std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m\n", .{ @as(u32, @intFromFloat(col_neg_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_neg_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_neg_rgb.rgb.b * 255)) });
-    }
-    std.debug.print("\n", .{});
+    // std.debug.print("Loading palette...\n", .{});
+    // var start: i64 = std.time.milliTimestamp();
+    // const pal: palette.Palette = try cache.readPaletteCache(allocator, conf.image_path) orelse try palette.Palette.init(allocator, conf.image_path);
+    // defer pal.deinit(allocator);
+    // try cache.writePaletteCache(allocator, &pal);
+    // std.debug.print("Creating new palette took {}ms \n", .{std.time.milliTimestamp() - start});
+    // // Check if image is light or dark themed
+    // const is_palette_light: bool = if (conf.light_mode != null) conf.light_mode.? else pal.isLight();
+    // std.debug.print("Image is in {s} theme\n", .{if (is_palette_light) "light" else "dark"});
+    // // Get clustering data
+    // std.debug.print("Generating clusters...\n", .{});
+    // start = std.time.milliTimestamp();
+    // const clusters: []color.Color = try clustering.kmeans(allocator, &pal, 4, 50);
+    // defer allocator.free(clusters);
+    // std.debug.print("Generating clusters took {}ms \n", .{std.time.milliTimestamp() - start});
+    // // Sort based on color theme
+    // const sort_ctx = struct { light_mode: bool };
+    // std.sort.block(color.Color, clusters, sort_ctx{ .light_mode = is_palette_light }, struct {
+    //     pub fn lessThan(ctx: sort_ctx, a: color.Color, b: color.Color) bool {
+    //         return if (ctx.light_mode) a.getBrightness() > b.getBrightness() else a.getBrightness() < b.getBrightness();
+    //     }
+    // }.lessThan);
+    // // Create the modulation curve for accent colors
+    // const test_curve: modulation_curve.ModulationCurve = modulation_curve.ModulationCurve.init(.hsl, &.{
+    //     .{ .a_mod = null, .b_mod = 0.98, .c_mod = 0.09 },
+    //     .{ .a_mod = null, .b_mod = 0.94, .c_mod = 0.16 },
+    //     .{ .a_mod = null, .b_mod = 0.90, .c_mod = 0.25 },
+    //     .{ .a_mod = null, .b_mod = 0.82, .c_mod = 0.30 },
+    //     .{ .a_mod = null, .b_mod = 0.67, .c_mod = 0.42 },
+    //     .{ .a_mod = null, .b_mod = 0.68, .c_mod = 0.62 },
+    //     .{ .a_mod = null, .b_mod = 0.76, .c_mod = 0.75 },
+    //     .{ .a_mod = null, .b_mod = 0.92, .c_mod = 0.87 },
+    // });
+    // // Do stuff
+    // for (clusters) |*col| {
+    //     // Primary color
+    //     const col_rgb: color.Color = col.toRGB();
+    //     std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m", .{ @as(u32, @intFromFloat(col_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_rgb.rgb.b * 255)) });
+    //     // Accent colors
+    //     const new_cols: []color.Color = try test_curve.applyCurve(allocator, col);
+    //     defer allocator.free(new_cols);
+    //     std.sort.block(color.Color, new_cols, sort_ctx{ .light_mode = is_palette_light }, struct {
+    //         pub fn lessThan(ctx: sort_ctx, a: color.Color, b: color.Color) bool {
+    //             return if (ctx.light_mode) a.getBrightness() > b.getBrightness() else a.getBrightness() < b.getBrightness();
+    //         }
+    //     }.lessThan);
+    //     for (new_cols) |*col_acc| {
+    //         const col_acc_rgb: color.Color = col_acc.toRGB();
+    //         std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m", .{ @as(u32, @intFromFloat(col_acc_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_acc_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_acc_rgb.rgb.b * 255)) });
+    //     }
+    //     // Text color
+    //     var col_neg_hsl: color.Color = col.negative().toHSL();
+    //     col_neg_hsl.hsl.s = 0.1;
+    //     col_neg_hsl.hsl.l = if (is_palette_light) 0.01 else 0.99;
+    //     const col_neg_rgb: color.Color = col_neg_hsl.toRGB();
+    //     std.debug.print("\x1B[48;2;{};{};{}m     \x1B[0m\n", .{ @as(u32, @intFromFloat(col_neg_rgb.rgb.r * 255)), @as(u32, @intFromFloat(col_neg_rgb.rgb.g * 255)), @as(u32, @intFromFloat(col_neg_rgb.rgb.b * 255)) });
+    // }
+    // std.debug.print("\n", .{});
 }
