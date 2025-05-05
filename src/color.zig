344ed7799d53394ef650f83c5b3216b17f2907b5
Fuwuffy <palazzini21@gmail.com>
2025-05-05 15:25:07 +0200

    Improve color code organization

--- src/color.zig
+++ src/color.zig
@@ -2,6 +2,41 @@ const std = @import("std");
 
 const Vec3 = @Vector(3, f32);
 
+const OneVec = @as(Vec3, @splat(1.0));
+const ZeroVec = @as(Vec3, @splat(0.0));
+const Epsilon = 1e-6;
+// sRGB - linear RGB thresholds & coeffs
+const SRGB_Threshold = 0.04045;
+const SRGB_Linear_Factor = 12.92;
+const SRGB_Linear_Exp = 2.4;
+const SRGB_Inv_Linear_Exp = 1.0 / SRGB_Linear_Exp;
+const SRGB_Scale = 1.055;
+const SRGB_Offset = 0.055;
+// CIE reference white
+const D65: [3]f32 = .{ 0.95047, 1.00000, 1.08883 };
+// Color‐distance weights
+const ColorWeightsRGB: Vec3 = .{ 0.2126, 0.7152, 0.0722 };
+// RGB → XYZ
+const XYZ_Coeff: struct { x: Vec3, y: Vec3, z: Vec3 } = .{
+    .x = .{ 0.4124, 0.3576, 0.1805 },
+    .y = .{ 0.2126, 0.7152, 0.0722 },
+    .z = .{ 0.0193, 0.1192, 0.9505 },
+};
+// XYZ → RGB
+const RGB_Coeff: struct { r: Vec3, g: Vec3, b: Vec3 } = .{
+    .r = .{  3.2406, -1.5372, -0.4986 },
+    .g = .{ -0.9689,  1.8758,  0.0415 },
+    .b = .{  0.0557, -0.2040,  1.0570 },
+};
+
+inline fn powVec(v: Vec3, exp: f32) Vec3 {
+    return .{
+        std.math.pow(f32, v[0], exp),
+        std.math.pow(f32, v[1], exp),
+        std.math.pow(f32, v[2], exp),
+    };
+}
+
 pub const ColorSpace = enum { rgb, hsl, xyz, lab };
 
 pub const Color = union(ColorSpace) {
@@ -12,13 +47,13 @@ pub const Color = union(ColorSpace) {
 
     pub inline fn values(self: *const @This()) [3]f32 {
         return switch (self.*) {
-            inline else => |c| c.values,
+            inline else => |c| c.values
         };
     }
 
-    pub inline fn setValues(self: *@This(), new_values: [3]f32) void {
+    pub inline fn setValues(self: *@This(), v: [3]f32) void {
         switch (self.*) {
-            inline else => |*c| c.values = new_values,
+            inline else => |*c| c.values = v
         }
     }
 
@@ -60,10 +95,10 @@ pub const Color = union(ColorSpace) {
 
     pub inline fn negative(self: Color) Color {
         return switch (self) {
-            .rgb => |rgb| .{ .rgb = rgb.negative() },
-            .hsl => |hsl| .{ .hsl = hsl.negative() },
-            .xyz => |xyz| .{ .xyz = xyz.negative() },
-            .lab => |lab| .{ .lab = lab.negative() },
+            .rgb => |c| .{ .rgb = c.negative() },
+            .hsl => |c| .{ .hsl = c.negative() },
+            .xyz => |c| .{ .xyz = c.negative() },
+            .lab => |c| .{ .lab = c.negative() },
         };
     }
 
@@ -74,84 +109,70 @@ pub const Color = union(ColorSpace) {
     }
 
     pub inline fn dst(self: *const @This(), other: *const Color) f32 {
-        const self_tag = std.meta.activeTag(self.*);
-        const other_tag = std.meta.activeTag(other.*);
-        const other_converted: Color = if (self_tag == other_tag) other.* else switch (self.*) {
-            .rgb => other.toRGB(),
-            .hsl => other.toHSL(),
-            .xyz => other.toXYZ(),
-            .lab => other.toLAB(),
-        };
+        const tag = std.meta.activeTag(self.*);
+        const o: Color = if (tag == std.meta.activeTag(other.*)) other.* else
+            switch (self.*) {
+                .rgb => other.toRGB(),
+                .hsl => other.toHSL(),
+                .xyz => other.toXYZ(),
+                .lab => other.toLAB(),
+            };
         return switch (self.*) {
-            .rgb => self.rgb.dst(&other_converted.rgb),
-            .hsl => self.hsl.dst(&other_converted.hsl),
-            .xyz => self.xyz.dst(&other_converted.xyz),
-            .lab => self.lab.dst(&other_converted.lab),
+            .rgb => self.rgb.dst(&o.rgb),
+            .hsl => self.hsl.dst(&o.hsl),
+            .xyz => self.xyz.dst(&o.xyz),
+            .lab => self.lab.dst(&o.lab),
         };
     }
 };
 
 const ColorRGB = struct {
     values: Vec3,
-
-    // Used to slightly improve the percieved color in distance/brightness calculations
-    const ColorWeights: Vec3 = .{ 0.2126, 0.7152, 0.0722 };
-    // Used to convert RGB to XYZ colors
-    const XYZ_XCoeff: Vec3 = .{ 0.4124, 0.3576, 0.1805 };
-    const XYZ_YCoeff: Vec3 = .{ 0.2126, 0.7152, 0.0722 };
-    const XYZ_ZCoeff: Vec3 = .{ 0.0193, 0.1192, 0.9505 };
-
+    
     fn toHSL(self: *const @This()) ColorHSL {
-        const max: f32 = @reduce(.Max, self.values);
-        const min: f32 = @reduce(.Min, self.values);
-        const delta: f32 = max - min;
+        const mx: f32 = @reduce(.Max, self.values);
+        const mn: f32 = @reduce(.Min, self.values);
+        const d: f32 = mx - mn;
         var h: f32 = 0.0;
-        if (delta != 0) {
-            if (max == self.values[0]) {
-                h = (self.values[1] - self.values[2]) / delta;
-            } else if (max == self.values[1]) {
-                h = 2.0 + (self.values[2] - self.values[0]) / delta;
+        if (d > Epsilon) {
+            if (mx == self.values[0]) {
+                h = ((self.values[1] - self.values[2]) / d) * 60.0;
+            } else if (mx == self.values[1]) {
+                h = ((self.values[2] - self.values[0]) / d + 2.0) * 60.0;
             } else {
-                h = 4.0 + (self.values[0] - self.values[1]) / delta;
+                h = ((self.values[0] - self.values[1]) / d + 4.0) * 60.0;
             }
-            h *= 60.0;
         }
         h = @mod(h, 360.0);
-        const l: f32 = (max + min) / 2.0;
-        const s: f32 = if (delta == 0) 0.0 else delta / (1.0 - @abs(2.0 * l - 1.0));
+        const l: f32 = (mx + mn) * 0.5;
+        const s: f32 = if (d < Epsilon) 0.0 else d / (1.0 - @abs(2.0 * l - 1.0));
         return .{ .values = .{ h, s, l } };
     }
 
     fn toXYZ(self: *const @This()) ColorXYZ {
-        const mask: @Vector(3, bool) = self.values > @as(Vec3, @splat(0.04045));
-        var first: Vec3 = (self.values + @as(Vec3, @splat(0.055))) / @as(Vec3, @splat(1.055));
-        // FIXME: Workaround
-        first[0] = std.math.pow(f32, first[0], 2.4);
-        first[1] = std.math.pow(f32, first[1], 2.4);
-        first[2] = std.math.pow(f32, first[2], 2.4);
-        // End of workaround
-        const other: Vec3 = self.values / @as(Vec3, @splat(12.92));
-        const linear: Vec3 = @select(f32, mask, first, other);
-        return .{
-            .values = .{
-                @reduce(.Add, linear * XYZ_XCoeff),
-                @reduce(.Add, linear * XYZ_YCoeff),
-                @reduce(.Add, linear * XYZ_ZCoeff),
-            }
-        };
+        const mask: @Vector(3, bool) = self.values > @as(Vec3, @splat(SRGB_Threshold));
+        const lin: Vec3 = @select(f32, mask,
+            powVec((self.values + @as(Vec3, @splat(SRGB_Offset))) / @as(Vec3, @splat(SRGB_Scale)), SRGB_Linear_Exp),
+            self.values / @as(Vec3, @splat(SRGB_Linear_Factor))
+        );
+        return .{ .values = .{
+            @reduce(.Add, lin * XYZ_Coeff.x),
+            @reduce(.Add, lin * XYZ_Coeff.y),
+            @reduce(.Add, lin * XYZ_Coeff.z),
+        }};
     }
 
     fn dst(self: *const @This(), other: *const ColorRGB) f32 {
         const d: Vec3 = self.values - other.values;
-        return @reduce(.Add, (d * d) * ColorWeights);
+        return @reduce(.Add, d * d * ColorWeightsRGB);
     }
 
     fn negative(self: ColorRGB) ColorRGB {
-        return .{ .values = @as(Vec3, @splat(1.0)) - self.values };
+        return .{ .values = OneVec - self.values };
     }
 
     fn getBrightness(self: ColorRGB) f32 {
-        return @reduce(.Add, self.values * ColorWeights);
+        return @reduce(.Add, self.values * ColorWeightsRGB);
     }
 };
 
@@ -159,90 +180,82 @@ const ColorHSL = struct {
     values: Vec3,
 
     fn toRGB(self: *const @This()) ColorRGB {
+        const l: f32 = self.values[2];
         if (self.values[1] == 0.0) {
-            return .{ .values = @as(Vec3, @splat(self.values[2])) };
+            return .{ .values = @as(Vec3, @splat(l)) };
         }
-        const chroma: f32 = (1.0 - @abs(2.0 * self.values[2] - 1.0)) * self.values[1];
-        const h_prime: f32 = self.values[0] / 60.0;
-        const sector: u32 = @mod(@as(u32, @intFromFloat(h_prime)), 6);
-        const x: f32 = chroma * (1.0 - @abs(@mod(h_prime, 2.0) - 1.0));
-        const m: f32 = self.values[2] - chroma / 2.0;
-        var rgb: Vec3 = switch (sector) {
-            0 => .{ chroma, x, 0.0 },
-            1 => .{ x, chroma, 0.0 },
-            2 => .{ 0.0, chroma, x },
-            3 => .{ 0.0, x, chroma },
-            4 => .{ x, 0.0, chroma },
-            5 => .{ chroma, 0.0, x },
+        const c: f32 = (1 - @abs(2.0 * l - 1.0)) * self.values[1];
+        const hp: f32 = self.values[0] / 60.0;
+        const x: f32 = c * (1.0 - @abs(@mod(hp, 2.0) - 1.0));
+        const m: f32 = l - c * 0.5;
+        var rgb: Vec3 = switch (@mod(@as(u8, @intFromFloat(hp)), 6)) {
+            0 => Vec3{ c, x, 0 },
+            1 => Vec3{ x, c, 0 },
+            2 => Vec3{ 0, c, x },
+            3 => Vec3{ 0, x, c },
+            4 => Vec3{ x, 0, c },
+            5 => Vec3{ c, 0, x },
             else => unreachable,
-        };
-        rgb += @as(Vec3, @splat(m));
-        return .{ .values = @min(@max(rgb, @as(Vec3, @splat(0.0))), @as(Vec3, @splat(1.0))) };
+        } + @as(Vec3, @splat(m));
+        rgb = @min(@max(rgb, ZeroVec), OneVec);
+        return .{ .values = rgb };
+    }
+
+    fn dst(self: *const @This(), other: *const ColorHSL) f32 {
+        const a: f32 = std.math.degreesToRadians(self.values[0]);
+        const b: f32 = std.math.degreesToRadians(other.values[0]);
+        const u: Vec3 = .{ self.values[1] * @cos(a), self.values[1] * @sin(a), self.values[2] };
+        const v: Vec3 = .{ other.values[1] * @cos(b), other.values[1] * @sin(b), other.values[2] };
+        const d: Vec3 = u - v;
+        return @reduce(.Add, d * d) / 3.0;
     }
 
     fn negative(self: ColorHSL) ColorHSL {
-        return .{ .values = .{ self.values[0], self.values[1], 1.0 - self.values[2] } };
+        return .{ .values = .{ self.values[0], self.values[1], 1 - self.values[2] } };
     }
 
     fn getBrightness(self: ColorHSL) f32 {
         return self.values[2];
     }
-
-    fn dst(self: *const @This(), other: *const ColorHSL) f32 {
-        const h1_rad: f32 = std.math.degreesToRadians(self.values[0]);
-        const h2_rad: f32 = std.math.degreesToRadians(other.values[0]);
-        const vec1: Vec3 = .{ self.values[1] * @cos(h1_rad), self.values[1] * @sin(h1_rad), self.values[2] };
-        const vec2: Vec3 = .{ other.values[1] * @cos(h2_rad), other.values[1] * @sin(h2_rad), self.values[2] };
-        const d: Vec3 = vec1 - vec2;
-        return @reduce(.Add, d * d) / 3.0;
-    }
 };
 
 const ColorXYZ = struct {
     values: Vec3,
-    
-    // Used to convert XYZ to RGB colors
-    const RGB_RCoeff: Vec3 = .{ 3.2406, -1.5372, -0.4986 };
-    const RGB_GCoeff: Vec3 = .{ -0.9689, 1.8758, 0.0415 };
-    const RGB_BCoeff: Vec3 = .{ 0.0557, -0.2040, 1.0570 };
 
     fn toRGB(self: *const @This()) ColorRGB {
         const v: Vec3 = .{
-            @reduce(.Add, self.values * RGB_RCoeff),
-            @reduce(.Add, self.values * RGB_GCoeff),
-            @reduce(.Add, self.values * RGB_BCoeff),
+            @reduce(.Add, self.values * RGB_Coeff.r),
+            @reduce(.Add, self.values * RGB_Coeff.g),
+            @reduce(.Add, self.values * RGB_Coeff.b),
         };
         const mask: @Vector(3, bool) = v > @as(Vec3, @splat(0.0031308));
-        const first: Vec3 =
-            Vec3{
-                // FIXME: Workaround
-                std.math.pow(f32, v[0], 1.0 / 2.4),
-                std.math.pow(f32, v[1], 1.0 / 2.4),
-                std.math.pow(f32, v[2], 1.0 / 2.4),
-            } * @as(Vec3, @splat(1.055)) - @as(Vec3, @splat(0.055));
-        const other: Vec3 = v * @as(Vec3, @splat(12.92));
-        
-        const result: Vec3 = @select(f32, mask, first, other);
-        return .{ .values = @min(@max(result, @as(Vec3, @splat(0.0))), @as(Vec3, @splat(1.0))) };
+        var srgb: Vec3 = @select(f32,
+            mask,
+            powVec(v, SRGB_Inv_Linear_Exp) * @as(Vec3, @splat(SRGB_Scale)) - @as(Vec3, @splat(SRGB_Offset)),
+            v * @as(Vec3, @splat(SRGB_Linear_Factor))
+        );
+        srgb = @min(@max(srgb, ZeroVec), OneVec);
+        return .{ .values = srgb };
     }
 
     fn toLAB(self: *const @This()) ColorLAB {
-        const reference_white: [3]f32 = .{ 0.95047, 1.0, 1.08883 };
-        const referenced_color: Vec3 = self.values / reference_white;
-        const mask: @Vector(3, bool) = referenced_color > @as(Vec3, @splat(0.008856));
-        var first: Vec3 = referenced_color;
-        // FIXME: Workaround
-        first[0] = std.math.pow(f32, first[0], 1.0 / 3.0);
-        first[1] = std.math.pow(f32, first[1], 1.0 / 3.0);
-        first[2] = std.math.pow(f32, first[2], 1.0 / 3.0);
-        // End of workaround
-        const other: Vec3 = (referenced_color * @as(Vec3, @splat(903.3)) + @as(Vec3, @splat(16.0))) / @as(Vec3, @splat(116.0));
-        const f: Vec3 = @select(f32, mask, first, other);
+        const r: Vec3 = self.values / D65;
+        const mask: @Vector(3, bool) = r > @as(Vec3, @splat(0.008856));
+        const f: Vec3 = @select(f32,
+            mask,
+            powVec(r, 1.0 / 3.0),
+            (r * @as(Vec3, @splat(903.3)) + @as(Vec3, @splat(16.0))) / @as(Vec3, @splat(116.0))
+        );
         return .{ .values = .{
             116.0 * f[1] - 16.0,
             500.0 * (f[0] - f[1]),
             200.0 * (f[1] - f[2]),
-        } };
+        }};
+    }
+
+    fn dst(self: *const @This(), other: *const ColorXYZ) f32 {
+        const d: Vec3 = self.values - other.values;
+        return @reduce(.Add, d*d) / 3.0;
     }
 
     // TODO: Get better function
@@ -254,58 +267,41 @@ const ColorXYZ = struct {
     fn getBrightness(self: ColorXYZ) f32 {
         return self.toRGB().getBrightness();
     }
-
-    fn dst(self: *const @This(), other: *const ColorXYZ) f32 {
-        const d: Vec3 = self.values - other.values;
-        return @reduce(.Add, d * d) / 3.0;
-    }
 };
 
 const ColorLAB = struct {
     values: Vec3,
 
     fn toXYZ(self: *const @This()) ColorXYZ {
-        const reference_white: [3]f32 = .{ 0.95047, 1.0, 1.08883 };
-        
-        const fy: f32 = (self.values[0] + 16.0) / 116.0;
-        const fx: f32 = self.values[1] / 500.0 + fy;
-        const fz: f32 = fy - self.values[2] / 200.0;
-        
-        const xr: f32 = if (fx > 0.206897) fx * fx * fx else (116.0 * fx - 16.0) / 903.3;
-        const yr: f32 = if (self.values[0] > 7.9996) fy * fy * fy else self.values[0] / 903.3;
-        const zr: f32 = if (fz > 0.206897) fz * fz * fz else (116.0 * fz - 16.0) / 903.3;
-        
-        return .{
-            .values = .{
-                xr * reference_white[0],
-                yr * reference_white[1],
-                zr * reference_white[2],
-            }
-        };
-    }
-
-    fn negative(self: ColorLAB) ColorLAB {
-        return .{ .values = .{
-            100.0 - self.values[0],
-            -self.values[1],
-            -self.values[2],
-        } };
-    }
-
-    // TODO: Get better function
-    fn getBrightness(self: ColorLAB) f32 {
-        return self.toXYZ().getBrightness();
+        const fy: f32 = (self.values[0] + 16) / 116;
+        const fx: f32 = self.values[1] / 500 + fy;
+        const fz: f32 = fy - self.values[2] / 200;
+        const xr: f32 = if (fx > 0.206897) fx*fx*fx else (116*fx - 16) / 903.3;
+        const yr: f32 = if (self.values[0] > 7.9996) fy*fy*fy else self.values[0] / 903.3;
+        const zr: f32 = if (fz > 0.206897) fz*fz*fz else (116*fz - 16) / 903.3;
+        return .{ .values = .{ xr * D65[0], yr * D65[1], zr * D65[2] } };
     }
 
     fn dst(self: *const @This(), other: *const ColorLAB) f32 {
         const d: Vec3 = self.values - other.values;
-        return @reduce(.Add, d * d) * 7.08856e-6;
+        return @reduce(.Add, d*d) * 7.08856e-6;
+    }
+
+    fn negative(self: ColorLAB) ColorLAB {
+        return .{ .values = .{
+            100 - self.values[0],
+            -self.values[1],
+            -self.values[2],
+        }};
+    }
+
+    fn getBrightness(self: ColorLAB) f32 {
+        return self.values[0];
     }
 };
 
-const expectEqualSlices = std.testing.expectEqualSlices;
-
 test "color format conversions" {
+    const expectEqualSlices = std.testing.expectEqualSlices;
     // FIXME: Allow some bit of tolerance to tests, as values provided are not f32 precise lmao
     const colors_rgb: [8]Color = .{
         .{ .rgb = .{ .values = .{ 1, 0, 0 } } },
